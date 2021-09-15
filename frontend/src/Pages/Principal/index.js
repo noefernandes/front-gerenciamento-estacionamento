4baba4c3ece9c743d80431db60c2644834be2b66
Noé Fernandes <noefernandes@ufrn.edu.br>
2021-09-15 02:24:21 -0300

    Situação por vagas e sensores funcionais prontos na tela

--- frontend/src/Pages/Principal/index.js
+++ frontend/src/Pages/Principal/index.js
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 export default function Principal(){
+    const [estacionamentos, setEstacionamentos] = useState([]);
+    const [quantidadeSensores, setQuantidadeSensores] = useState();
+    const [sensoresDisponiveis, setSensoresDisponiveis] = useState(0);
+    const [capacidadeMaxima, setCapacidadeMaxima] = useState({});
+    const [ocupacao, setOcupacao] = useState({});
+    const [fluxo, setFluxo] = useState([]); 
+
+    useEffect(() => {
+        //Pegando os nomes dos setores e guardando no estado estacionamentos.
+        var nomeSetores = [];
+        api.get('api/estacionamentos').then(response => {
+            for(var i = 0; i < response.data.length; i++){
+                nomeSetores.push(response.data[i].setor)
+            }
+        })
+        
+        var capacidade = {};
+        var ocupacao = {};
+        var disponivel = 0;
+        var fluxo_temp = {};
+
+        //Armazenando no dicionário setores a quantidade de veículos
+        api.get('api/sensorVaga').then(response => {
+            for(var i = 0; i < nomeSetores.length; i++){
+                capacidade[nomeSetores[i]] = 0;
+                ocupacao[nomeSetores[i]] = 0;
+                for(var j = 0; j < response.data.length; j++){
+                    if(nomeSetores[i] === response.data[j].dispositivo.estacionamento.setor){
+                        capacidade[nomeSetores[i]] += 1; 
+                        if(response.data[j].ocupado === true)
+                            ocupacao[nomeSetores[i]] += 1;
+                    }
+                }
+            }
+
+            for(var i = 0; i < response.data.length; i++){
+                if(response.data[i].status === "Disponivel")
+                    disponivel += 1;
+            }
+
+            for(var key in capacidade){
+                if(capacidade[key] - ocupacao[key] === capacidade[key]){
+                    fluxo_temp[key] = "Livre";
+                }else if(capacidade[key] - ocupacao[key] === 0){
+                    fluxo_temp[key] = "Lotado";
+                }else{
+                    fluxo_temp[key] = "Moderado";
+                }
+            }
+
+            //console.log(capacidade);
+            //console.log(ocupacao);
+            //console.log(disponivel);
+            setCapacidadeMaxima(capacidade);
+            setOcupacao(ocupacao);
+            setSensoresDisponiveis(disponivel);
+            setQuantidadeSensores(response.data.length);
+            setFluxo(fluxo_temp);
+        })
+    }, []);
+    
     return (
         <div>
             <h1>Bem vindo ao GeVU</h1>
@@ -17,12 +79,13 @@ export default function Principal(){
 
             <h2>Situação de vagas por setor</h2>
             <div>
-                <p><b>Setor I:</b>  </p>
-                <p><b>Setor II:</b>  </p>
-                <p><b>Setor III:</b>  </p>
-                <p><b>Setor IV:</b>  </p>
-                <p><b>Setor V:</b>  </p>
-                <p><b>Setor IMD:</b>  </p>
+                <ul>
+                    {Object.keys(fluxo).map(valor => (
+                        <li>
+                            <p><b>{valor}</b>: {fluxo[valor]}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
 
             <h2>Quantidade de movimentações</h2>
@@ -33,8 +96,8 @@ export default function Principal(){
 
             <h2>Sensores funcionais</h2>
             <div>
-                <p>X sensores</p>
-                <p>X disponíveis</p>
+                <p>{quantidadeSensores} sensores</p>
+                <p>{sensoresDisponiveis} disponíveis</p>
             </div>
             
         </div>
